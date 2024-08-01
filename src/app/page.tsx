529113a9443b4c78f62ab4707c658b8e2f88ba33
Nicholas Hellmers <nihe1661@colorado.edu>
2024-08-01 16:06:39 -0600

    Scaffolded ui with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/70b649eb-fbda-4c80-9e89-a9fa421cec5b-cpf5u7.jpg",
+  "https://utfs.io/f/7d74030c-b84b-4955-a416-c14d87645ccf-v60it.webp",
+  "https://utfs.io/f/bf70e85e-0da8-4935-be14-70b3760bff5c-jc38m3.jpg",
+  "https://utfs.io/f/8c3572a2-39e9-4fae-bdc1-9f2ac2aae1b6-klfpmf.jpg",
+  "https://utfs.io/f/28fc2d2c-8a34-48fa-b5bf-99eb2562cd06-a6klyj.jpg"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: url + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+        <div className="flex flex-wrap gap-4"> {
+            [...mockImages, ...mockImages, ...mockImages].map((image) => (
+              <div key={image.id} className="w-48">
+                <img src={image.url} alt={image.id} />
+              </div>
+            ))
+          }
+        </div>
+
       Hello (in development)
     </main>
   );
